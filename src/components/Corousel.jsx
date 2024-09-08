fc2618e3432a4ad93f7383f5b74ced6f40bf8b01
Adithya S <141550319+aDiThYa-808@users.noreply.github.com>
2024-09-08 17:35:00 +0530

    Added favicion and make the corousel autoplay.

--- src/components/Corousel.jsx
+++ src/components/Corousel.jsx
@@ -8,7 +8,7 @@ function Corousel() {
   return (
     <>
       <div id='home' className="carousel-container">
-        <div id="carouselExampleCaptions" className="carousel slide">
+        <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
           <div className="carousel-indicators">
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
